0b816e8f5e1c1b3d73826114269ea280f9383647
Rafael França <hitalloexiled@hotmail.com>
2022-04-03 16:41:12 -0300

    chore: adds test and coverage configurations

---
+++ server/tests/embedded-document.spec.ts
@@ -0,0 +1,39 @@
+import chai                                 from "chai";
+import { suite, test }                      from "test-decorators";
+import { TextDocument, getLanguageService } from "vscode-html-languageservice";
+import EmbeddedDocument                     from "../internal/embedded-document.js";
+
+@suite
+export default class EmbeddedDocumentSpec
+{
+    @test
+    public empty(): void
+    {
+        const document = TextDocument.create("", "html", 1, "");
+
+        const regions = EmbeddedDocument.getRegions(getLanguageService(), document);
+
+        chai.assert.deepEqual(regions.getLanguagesInDocument(), ["html"]);
+    }
+
+    @test
+    public doSomething(): void
+    {
+        const content =
+        [
+            "<a style='color: blue' onclick='noop'></a>",
+            "<style>",
+            "   h1 { color: red; }",
+            "</style>",
+            "<script>",
+            "   alert('dont use alert');",
+            "</script>",
+        ].join("\n");
+
+        const document = TextDocument.create("", "html", 1, content);
+
+        const regions = EmbeddedDocument.getRegions(getLanguageService(), document);
+
+        chai.assert.deepEqual(regions.getLanguagesInDocument(), ["css", "javascript", "html"]);
+    }
+}
